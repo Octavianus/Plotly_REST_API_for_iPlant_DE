4c6ff28a8d0806d3eb5eb29b62902bdc037ea0d1
David Weng <david90@IPC-COLDEN.local>
2014-08-11 18:17:11 -0700

    a

---
+++ src/main/java/plotly/ConnectionGet.java
@@ -0,0 +1,182 @@
+package main.java.plotly;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ConnectionGet {
+
+		private String un = "";
+		private String platform = "";
+		private String email = "";
+		
+		public ConnectionGet(){
+		}
+		
+		// this data already given by iplant if only one user
+		public ConnectionGet(String un, String email, String platform) {
+			this.un = un;
+			this.email = email;
+			this.platform = platform;
+		}
+		
+		public static void main(String[] args) throws Exception{
+			// Constructor of the connection
+			// TODO get the data from UI
+			// If given by iplant, only one account.
+			String un = "david90test";
+			String email = "wxdsdtc@gmail.com";
+			String platform = "java";
+			String outPutFile = "plots.csv";
+			String SelectingFile = "gene_exp.csv";
+			
+			ConnectionGet conn = new ConnectionGet(un, email, platform);
+			
+			UserData getPara = new UserData();
+			getPara.setEmail(email);
+			getPara.setUsername(un);
+			getPara.setPlatform(platform);
+			
+			// Test 1 -- for new user
+			String urlParameters1 = getPara.getURLParemeters();
+			System.out.println("Testing 1 - Register a new user in plotly");
+			conn.New_User(urlParameters1);
+			
+			// Test 2 -- for new plot
+			// TODO selectingColumn from iplant UI
+			getPara.select(SelectingFile, outPutFile /* selectingColumn*/);
+			String urlParameters2 = getPara.getURLParemeters(SelectingFile, outPutFile);
+			System.out.println("Testing 2 - Send a plot request to plotly");
+			conn.Plot_Data(urlParameters2);
+			
+		}
+		
+		// Register a new user from plotly.
+		private void New_User(String urlParameters) throws Exception{
+			// Use plotly rest api to request a new user account.
+			try{
+				// Set up a plotly account with the needed parameters.
+				String url = "https://plot.ly/apimkacct";
+				
+				URL obj = new URL(url);
+				// There is a way to install unirest-java with Maven. 
+				// Send post request
+				HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Accept", "application/json");
+				conn.setDoOutput(true);
+				DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+				wr.writeBytes(urlParameters);
+				wr.flush();
+				wr.close();
+		 
+				if(conn.getResponseCode() > 200){
+					throw new RuntimeException("Failed : HTTP error code:" + conn.getResponseCode());
+				}
+				// TODO if the result is null, throw error
+				
+				// Temp strings to store the reply from plotly.
+				String fullOutPut = "";
+				String oneLine = "";
+				
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						(conn.getInputStream())));
+				
+				while((oneLine = br.readLine()) != null){
+					//test
+					System.out.println("Json Msg:" + oneLine);
+					fullOutPut += oneLine;
+				}
+				
+				// Read value from the json format response 
+				// temp strings to store the result from the returned response.
+				String returnMsg = "";
+				String tmp_pw = "";
+				String key = "";
+				String api_key = "";
+				// TODO Give values to the above from the returned buffer br which in 
+				// json format. And judge if there are msg or error. 
+				// few places http://json.org/java/, net.sf.json for more places in the backend.
+				
+				System.out.println("Json Msg:" + fullOutPut);
+				
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// Provide with required parameters to plotly. 
+		private void Plot_Data(String urlParameters) throws Exception{	
+			// TODO Read and determine the rows and columns in file to the url parameters. 
+			
+			// User input of a input and temp output file.
+			
+			// Return a series of strings
+			// UserData process = new UserData();
+			//process.select(SelectingFile, outPutFile);
+			
+			/*
+			 * Seccesfully check return a api based on the parameters
+			 */
+			try{
+				// Set up a plotly account with the needed parameters.
+				String url = "https://plot.ly/clientresp";
+				// TODO separate apart the parameters based on user input. 
+				//String urlParameters1 = "un=david90test&key=5afprolg26&origin=plot&platform=Java&args=[[0, 1, 2], [3, 4, 5], [1, 2, 3], [6, 6, 5]]&kwargs={\"filename\": \"plot from api\",\"fileopt\": \"overwrite\",\"style\": {\"type\": \"bar\"},\"traces\": [1],\"layout\": {\"title\": \"experimental data\"},\"world_readable\": true}";
+				
+				URL obj = new URL(url);
+				// There is a way to install unirest-java with Maven. 
+				// Send post request
+				HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Accept", "application/json");
+				conn.setDoOutput(true);
+				DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+				wr.writeBytes(urlParameters);
+				wr.flush();
+				wr.close();
+		 
+				if(conn.getResponseCode() > 200){
+					throw new RuntimeException("Failed : HTTP error code:" + conn.getResponseCode());
+				}
+				// TODO if the result is null, throw error
+				
+				// Temp strings to store the reply from plotly.
+				String fullOutPut = "";
+				String oneLine = "";
+				
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						(conn.getInputStream())));
+				
+				while((oneLine = br.readLine()) != null){
+					//test
+					System.out.println("Json Msg:" + oneLine);
+					fullOutPut += oneLine;
+				}
+				
+				// Read value from the json format response 
+				// temp strings to store the result of the returned response.
+				// TODO Give values to the above from the returned buffer br which in 
+				// json format. And judge if there are msg or error. http://json.org/java/
+				
+				System.out.println("Json Msg:" + fullOutPut);
+				
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			/*
+			*/
+			
+		}
+}
---
+++ src/main/java/plotly/UserData.java
@@ -0,0 +1,251 @@
+package main.java.plotly;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+import net.sf.json.*;
+
+public class UserData {
+	
+	// File
+	private String SelectingFile;
+	private String outPutFile;
+	
+	String urlParameters2 = "un=david90test&key=5afprolg26&origin=plot&"
+			+ "platform=Java&args=[[0, 1, 2], [3, 4, 5], [1, 2, 3], [6, 6, 5]]&"
+			+ "kwargs={\"filename\": \"plot from api\",\"fileopt\": \"overwrite\",\"style\": {\"type\": \"bar\"},\"traces\": [1],\"layout\": {\"title\": \"experimental data\"},\"world_readable\": true}";
+	
+	// New User
+	private String username = "un=";
+	private String email = "email=";
+	private String platform = "platform=";
+	private String tmp_pw = "tmp_pw=";
+	// private String version = "version=0.1";
+	
+	// Plot Data
+	// api_key is default, given by iplant
+	private String api_key = "key=5afprolg26";
+	// origin = plot as default
+	private String origin = "origin=plot";
+	private String args = "args=";
+	private String kwargs = "kwargs=";
+	private String url = "";
+	private String message = "";
+	private String warning = "Warning message: ";
+	private String error = "Error message: ";
+	private String filename = "";
+	
+	/* 
+	 * The followings set up all the parameters.
+	 */
+	public UserData() {
+	}
+	
+	public UserData(String SelectingFile) {
+		this.SelectingFile = SelectingFile;
+	}
+	
+	public String getSelectingFile(){
+		return this.SelectingFile;
+	}
+	
+	public void setOutPutFile(String outPutFile){
+		this.outPutFile = outPutFile; 
+	}
+	
+	public String getOutPutFile(){
+		return this.outPutFile;
+	}
+	
+	public void  set_api_key(String set_api_key){
+		this.api_key += set_api_key;
+	}
+	
+	public String get_api_key(){
+		return this.api_key;
+	}
+	
+	public void  setUsername(String user_name){
+		this.username += user_name;
+	}
+	
+	public String getUsername(){
+		return this.username;
+	}
+
+	public void  setEmail(String Email){
+		this.email += Email;
+	}
+	
+	public String getEmail(){
+		return this.email;
+	}
+	
+	public void  setURL(String url){
+		this.url = url;
+	}
+	
+	public String getURL(){
+		return this.url;
+	}
+
+	public void  setPlatform(String platform){
+		this.platform += platform;
+	}
+	
+	public String getPlatform(){
+		return this.platform;
+	}
+	
+	public void  setPwd(String pwd){
+		this.tmp_pw += pwd;
+	}
+	
+	public String getPwd(){
+		return this.tmp_pw;
+	}
+	
+	/*	Get Set
+	 *  private String message = "";
+	 *	private String warning = "Warning message: ";
+	 *	private String error = "Error message: ";
+	 *	private String filename = "";
+	 */
+	
+	/* 
+	 * save the columns which are selecting into a temp file
+	 */
+	public void select(String in, String out) throws Exception{
+		try {
+			// Try to read the CSV files locally.
+			// TODO Read from iRODS and UI
+			String sig = "significant";
+			
+			List<String> selectingColumn = asList("gene","sample_1","sample_2","value_1","value_2","significant");
+			List<Integer> indexofSelectingColumns = new ArrayList<Integer>();
+			
+			CSVReader reader = new CSVReader(new FileReader(in));
+			CSVWriter writer = new CSVWriter(new FileWriter(out));
+			
+			List<String[]> rs = reader.readAll();
+			// Get the header of the file, and check the significant field.
+			String[] header = rs.get(0);
+			
+			// If there is a column marks the significant, filter the rows that are
+			// not significant in order to save the executing time.
+			// Also record the index of all the selecting columns.
+			
+			// TODO user could choice if they need to filter the insignificant rows. 
+			// Reduce disk I/O by editing the file in the RAM all the time.
+			int indexofHeader = 0;
+			boolean removeInsig = true;
+			while(indexofHeader < header.length){
+				/*
+				 * Select the rows here
+				 */
+				if(header[indexofHeader].matches(sig) && removeInsig)
+				{
+					// Scan start from the data, remove the rows that are insignificant.
+					int indexofRows = 1;
+					while(indexofRows < rs.size()){
+						if(rs.get(indexofRows)[indexofHeader].matches("no")){
+							// System.out.println(rs.remove(indexofRows));
+							rs.remove(indexofRows);
+							// -- Tricky bug since the size has been trimmed.
+							indexofRows --;
+							// System.out.println(indexofRows);
+						}
+						indexofRows ++;
+					}
+				}
+				
+				/*
+				 * Record the index of all the selecting columns.
+				 */
+				
+				Iterator<String> it = selectingColumn.iterator();
+				while(it.hasNext()){
+					if(header[indexofHeader].matches(it.next()))
+						indexofSelectingColumns.add(indexofHeader);
+				}
+				
+				indexofHeader ++;
+			}
+			
+			/*
+			 * Select the columns here
+			 */
+			int indexofRows = 0;
+			List<String[]> outputFile = new ArrayList<String[]>();
+			while(indexofRows < rs.size())
+			{
+				Iterator<Integer> it = indexofSelectingColumns.iterator();
+				String[] entries;
+				String oneLine = "";
+				while(it.hasNext())
+				{
+					oneLine += rs.get(indexofRows)[it.next()];
+					if(it.hasNext())
+						oneLine += "#";
+				}
+				
+				entries = oneLine.split("#");
+				outputFile.add(entries);
+				indexofRows ++;
+			}
+			
+			writer.writeAll(outputFile);
+			
+			// Clean the butt
+			reader.close();
+			writer.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO file not found error code or msg returned.
+			System.out.println("File not found");
+			e.printStackTrace();
+		}
+		
+	}	
+	
+	/* 
+	 * Get the url of POST request 
+	 */
+	public String getURLParemeters() throws Exception{
+		String URLParameters = username +"&"+ email +"&"+ platform;
+		return URLParameters;
+	}
+	
+	/*
+	 * Overload the for POST plot method 
+	 */
+	public String getURLParemeters(String in, String out) throws Exception{
+		String URLParameters = "";
+		CSVReader reader = new CSVReader( new FileReader(out));
+		List<String[]> rs = reader.readAll();
+		String[] header = rs.get(0);
+		
+		String urlParameters2 = "un=david90test&key=5afprolg26&origin=plot&"
+				+ "platform=Java"
+				+ "&args=[[0, 1, 2], [3, 4, 5], [1, 2, 3], [6, 6, 5]]&"
+				+ "kwargs={\"filename\": \"plot from api\",\"fileopt\": \"overwrite\",\"style\": {\"type\": \"bar\"},\"traces\": [1],\"layout\": {\"title\": \"experimental data\"},\"world_readable\": true}";
+		
+		
+		String prefix_data = username +"&"+ api_key + "&" + email +"&" + origin + "&" + platform;
+		//String data 
+		//String butt_data = 
+		
+		return urlParameters2;
+	}
+	
+
+
+}
